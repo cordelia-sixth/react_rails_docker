08590d2a25346e940f1a3d91e2da7fe9b8e6b3e4
cordelia <cordeliasixth@gmail.com>
2021-03-08 02:08:05 +0900

    feat: basic React flow

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,28 +1,47 @@
+const HtmlwebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   mode: "development",
-  entry: "./src/entry.js",
+  entry: "./src/index.jsx",
 
+  // // bundle fileの格納先を指定
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: 'bundle.js'
+  // },
+
+  // webpack-dev-serverの設定
   devServer: {
-    compress: true,
-    host: '0.0.0.0',
-    port: 8080
-  }
+    compress: true, // ファイルを圧縮する
+    hot: true,
+    host: '0.0.0.0', // hostを指定する
+    port: 8080 //　待ち受けportを指定する
+  },
 
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.tsx?$/,
-  //       use: "ts-loader"
-  //     },
-  //     // {
-  //     //   test: /\.jsx?$/,
-  //     //   loader:'babel-loader',
-  //     // }
-  //   ]
-  // },
-  // resolve: {
-  //   extensions: [
-  //     ".ts", ".tsx", ".js", ".jsx"
-  //   ]
-  // },
+  module: {
+    rules: [
+      // {
+      //   test: /\.tsx?$/,
+      //   use: "ts-loader"
+      // },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlwebpackPlugin({
+        template: './src/index.html'
+    })
+  ],
+
+  // import分でファイルを指定する時に拡張子を省略する
+  resolve: {
+    extensions: [
+      ".js", ".jsx"
+    ]
+  }
 };
